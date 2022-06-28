cb895cad08afc3a96593cfd591939d38274c2334
Supreet Pradhan <73893884+Supp-2020@users.noreply.github.com>
2022-06-28 17:06:00 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Loader = () => {
-  return(         // fas fa-spinner is type of spinner and its class name %% fa-4x makes it 4 times bigger %% fa-spin makes it spin for animation  
+  return(         
     <div className="loader">    
      <i class="fa fa-spinner fa-4x fa-spin"></i>
     </div>
